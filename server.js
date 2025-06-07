3060c2165f377956463c9bf6ba016fd1d93bf24a
Dipen <dipendraboyaju222@gmail.com>
2025-06-07 10:40:50 +0545

    ch

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors({
   // origin: 'http://localhost:5173',
   // origin: 'https://prateek-1.vercel.app',
-  origin: 'https://e2fc0427.sitepreview.org/',
+  origin: 'https://e2fc0427.sitepreview.org',
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: true,
 }));
