6d73edd3d356eee0bdaa4ab2e2a09bc20b890fd4
Dipen <dipendraboyaju222@gmail.com>
2025-07-14 11:41:12 +0545

    package updated

--- routes/newsletterRoute.js
+++ routes/newsletterRoute.js
@@ -4,7 +4,7 @@ import { uploadPdf } from '../middleware/multerMiddleware.js'
 
 const router = express.Router()
 
-router.post("/newsletter/upload", uploadPdf, uploadNewsletter);
+// router.post("/newsletter/upload", uploadPdf, uploadNewsletter);
 router.get("/newsletter/all", getAllNewsletters);
 router.delete("/newsletter/deleteNewsletter/:id", deleteNewsletter);
 router.patch('/newsletter/publishStatus/:id', publishStatus);
